b45078b2b799c3c81a905f0304051d890c420c31
José García <joselitokn2@hotmail.com>
2019-11-18 16:53:50 +0100

    ejrecicios unidad8

---
+++ UD8/ejercicios_clases.js
@@ -0,0 +1,13 @@
+/* CLASES EN JS
+
+    1. Diseña una clase colegio. Tendrá como atributos nombre, numero de aulas y alumnos.
+       Cada alumno se representará como una objeto de la clase Alumno. En ella se guardaran los atributos “DNI”, “nombre” y “nota_media”.
+       Implementa métodos en colegio y alumno para modificar la nota media de un alumno y que nos permita consultar la media de todo el colegio y cuántos alumnos hay.
+       
+    2. Diseña una clase Aeropuerto. Tendrá como atributos nombre, ciudad y numero de vuelos diarios nacionales.
+       Cada vuelo diario se representará como un objeto de la clase Vuelo. En ella se guardaran los atributos “codigo”, “hora_llegada” y “hora_salida”.
+       Implementa métodos en aeropuerto y vuelo para modificar la hora de llegada, para modificar la hora de salida y para comprobar si la hora de salida es posterior a la hora de llegada.
+       
+    3. Diseña una clase Hospital. Tendrá como atributos “nombre”, “ciudad” y “num_pacientes” (numero de pacientes ingresados).
+       Cada paciente se representará como una objeto de la clase Paciente. En ella se guardaran los atributos “DNI”, “nombre”, “enfermedad”.
+       Implementa un método en Hospital que reciba el código de paciente y se le de el alta a dicho paciencia (equivale a eliminar al paciente). */
